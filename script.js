a55a4232ac1e47cf63b04080fe0686ccdd2d9c76
Haliunaa Munkhuu <42748002+khaliun20@users.noreply.github.com>
2024-05-27 18:37:37 -0400

    add education and work

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+  function toggleResume() {
+    var resume = document.querySelector('.old-resume');
+    resume.classList.toggle('expanded');
+    
+    var expandButton = document.querySelector('.expand-button');
+    if (resume.classList.contains('expanded')) {
+      expandButton.textContent = 'Hide';
+    } else {
+      expandButton.textContent = 'Other Work Experiences';
+    }
+  }
